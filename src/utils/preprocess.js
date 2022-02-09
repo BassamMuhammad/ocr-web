b8b786ac2c54835af32f0b5969566379c51cdf82
BassamMuhammad <72605456+BassamMuhammad@users.noreply.github.com>
2022-02-10 00:25:19 +0500

    change

--- src/utils/preprocess.js
+++ src/utils/preprocess.js
@@ -200,13 +200,13 @@ function blurARGB(pixels, canvas, radius) {
   }
   setPixels(pixels, argb);
 }
-function invertColors(pixels) {
-  for (var i = 0; i < pixels.length; i += 4) {
-    pixels[i] = pixels[i] ^ 255; // Invert Red
-    pixels[i + 1] = pixels[i + 1] ^ 255; // Invert Green
-    pixels[i + 2] = pixels[i + 2] ^ 255; // Invert Blue
-  }
-}
+// function invertColors(pixels) {
+//   for (var i = 0; i < pixels.length; i += 4) {
+//     pixels[i] = pixels[i] ^ 255; // Invert Red
+//     pixels[i + 1] = pixels[i + 1] ^ 255; // Invert Green
+//     pixels[i + 2] = pixels[i + 2] ^ 255; // Invert Blue
+//   }
+// }
 function dilate(pixels, canvas) {
   let currIdx = 0;
   const maxIdx = pixels.length ? pixels.length / 4 : 0;
