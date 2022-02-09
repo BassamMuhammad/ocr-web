1f0840918a8952e5575da463788a1e2707d718c1
BassamMuhammad <72605456+BassamMuhammad@users.noreply.github.com>
2022-02-10 00:30:54 +0500

    change

--- src/utils/preprocess.js
+++ src/utils/preprocess.js
@@ -15,7 +15,7 @@ export async function preprocessImage(img) {
       canvas.height
     );
     blurARGB(processedImageData.data, canvas, 1);
-    dilate(processedImageData.data, canvas);
+    // dilate(processedImageData.data, canvas);
     // invertColors(processedImageData.data);
     thresholdFilter(processedImageData.data, 0.4);
     ctx.putImageData(processedImageData, 0, 0);
@@ -207,83 +207,83 @@ function blurARGB(pixels, canvas, radius) {
 //     pixels[i + 2] = pixels[i + 2] ^ 255; // Invert Blue
 //   }
 // }
-function dilate(pixels, canvas) {
-  let currIdx = 0;
-  const maxIdx = pixels.length ? pixels.length / 4 : 0;
-  const out = new Int32Array(maxIdx);
-  let currRowIdx, maxRowIdx, colOrig, colOut, currLum;
+// function dilate(pixels, canvas) {
+//   let currIdx = 0;
+//   const maxIdx = pixels.length ? pixels.length / 4 : 0;
+//   const out = new Int32Array(maxIdx);
+//   let currRowIdx, maxRowIdx, colOrig, colOut, currLum;
 
-  let idxRight, idxLeft, idxUp, idxDown;
-  let colRight, colLeft, colUp, colDown;
-  let lumRight, lumLeft, lumUp, lumDown;
+//   let idxRight, idxLeft, idxUp, idxDown;
+//   let colRight, colLeft, colUp, colDown;
+//   let lumRight, lumLeft, lumUp, lumDown;
 
-  while (currIdx < maxIdx) {
-    currRowIdx = currIdx;
-    maxRowIdx = currIdx + canvas.width;
-    while (currIdx < maxRowIdx) {
-      colOrig = colOut = getARGB(pixels, currIdx);
-      idxLeft = currIdx - 1;
-      idxRight = currIdx + 1;
-      idxUp = currIdx - canvas.width;
-      idxDown = currIdx + canvas.width;
+//   while (currIdx < maxIdx) {
+//     currRowIdx = currIdx;
+//     maxRowIdx = currIdx + canvas.width;
+//     while (currIdx < maxRowIdx) {
+//       colOrig = colOut = getARGB(pixels, currIdx);
+//       idxLeft = currIdx - 1;
+//       idxRight = currIdx + 1;
+//       idxUp = currIdx - canvas.width;
+//       idxDown = currIdx + canvas.width;
 
-      if (idxLeft < currRowIdx) {
-        idxLeft = currIdx;
-      }
-      if (idxRight >= maxRowIdx) {
-        idxRight = currIdx;
-      }
-      if (idxUp < 0) {
-        idxUp = 0;
-      }
-      if (idxDown >= maxIdx) {
-        idxDown = currIdx;
-      }
-      colUp = getARGB(pixels, idxUp);
-      colLeft = getARGB(pixels, idxLeft);
-      colDown = getARGB(pixels, idxDown);
-      colRight = getARGB(pixels, idxRight);
+//       if (idxLeft < currRowIdx) {
+//         idxLeft = currIdx;
+//       }
+//       if (idxRight >= maxRowIdx) {
+//         idxRight = currIdx;
+//       }
+//       if (idxUp < 0) {
+//         idxUp = 0;
+//       }
+//       if (idxDown >= maxIdx) {
+//         idxDown = currIdx;
+//       }
+//       colUp = getARGB(pixels, idxUp);
+//       colLeft = getARGB(pixels, idxLeft);
+//       colDown = getARGB(pixels, idxDown);
+//       colRight = getARGB(pixels, idxRight);
 
-      //compute luminance
-      currLum =
-        77 * ((colOrig >> 16) & 0xff) +
-        151 * ((colOrig >> 8) & 0xff) +
-        28 * (colOrig & 0xff);
-      lumLeft =
-        77 * ((colLeft >> 16) & 0xff) +
-        151 * ((colLeft >> 8) & 0xff) +
-        28 * (colLeft & 0xff);
-      lumRight =
-        77 * ((colRight >> 16) & 0xff) +
-        151 * ((colRight >> 8) & 0xff) +
-        28 * (colRight & 0xff);
-      lumUp =
-        77 * ((colUp >> 16) & 0xff) +
-        151 * ((colUp >> 8) & 0xff) +
-        28 * (colUp & 0xff);
-      lumDown =
-        77 * ((colDown >> 16) & 0xff) +
-        151 * ((colDown >> 8) & 0xff) +
-        28 * (colDown & 0xff);
+//       //compute luminance
+//       currLum =
+//         77 * ((colOrig >> 16) & 0xff) +
+//         151 * ((colOrig >> 8) & 0xff) +
+//         28 * (colOrig & 0xff);
+//       lumLeft =
+//         77 * ((colLeft >> 16) & 0xff) +
+//         151 * ((colLeft >> 8) & 0xff) +
+//         28 * (colLeft & 0xff);
+//       lumRight =
+//         77 * ((colRight >> 16) & 0xff) +
+//         151 * ((colRight >> 8) & 0xff) +
+//         28 * (colRight & 0xff);
+//       lumUp =
+//         77 * ((colUp >> 16) & 0xff) +
+//         151 * ((colUp >> 8) & 0xff) +
+//         28 * (colUp & 0xff);
+//       lumDown =
+//         77 * ((colDown >> 16) & 0xff) +
+//         151 * ((colDown >> 8) & 0xff) +
+//         28 * (colDown & 0xff);
 
-      if (lumLeft > currLum) {
-        colOut = colLeft;
-        currLum = lumLeft;
-      }
-      if (lumRight > currLum) {
-        colOut = colRight;
-        currLum = lumRight;
-      }
-      if (lumUp > currLum) {
-        colOut = colUp;
-        currLum = lumUp;
-      }
-      if (lumDown > currLum) {
-        colOut = colDown;
-        currLum = lumDown;
-      }
-      out[currIdx++] = colOut;
-    }
-  }
-  setPixels(pixels, out);
-}
+//       if (lumLeft > currLum) {
+//         colOut = colLeft;
+//         currLum = lumLeft;
+//       }
+//       if (lumRight > currLum) {
+//         colOut = colRight;
+//         currLum = lumRight;
+//       }
+//       if (lumUp > currLum) {
+//         colOut = colUp;
+//         currLum = lumUp;
+//       }
+//       if (lumDown > currLum) {
+//         colOut = colDown;
+//         currLum = lumDown;
+//       }
+//       out[currIdx++] = colOut;
+//     }
+//   }
+//   setPixels(pixels, out);
+// }
