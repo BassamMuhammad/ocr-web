472a1e4a5064af5aeab658231179da8ae6e0a405
BassamMuhammad <72605456+BassamMuhammad@users.noreply.github.com>
2022-02-10 00:47:17 +0500

    .

--- src/utils/preprocess.js
+++ src/utils/preprocess.js
@@ -17,7 +17,7 @@ export async function preprocessImage(img) {
     blurARGB(processedImageData.data, canvas, 1);
     // dilate(processedImageData.data, canvas);
     // invertColors(processedImageData.data);
-    thresholdFilter(processedImageData.data, 0.42);
+    thresholdFilter(processedImageData.data, 0.4);
     ctx.putImageData(processedImageData, 0, 0);
   };
   await new Promise((r) => setTimeout(r, 1000));
